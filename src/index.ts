00dd7136735a094f1cc40be6a21ba02a00943907
cnaranjo <cnaranjo@altioracorp.com>
2020-03-05 09:42:00 -0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {PharmacyServerApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {PharmacyServerApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new PharmacyServerApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
